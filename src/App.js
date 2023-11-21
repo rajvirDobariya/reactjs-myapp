25209415e87c942e7b03205e24319855b72102f5
umesh goti <mailto:gotiumesh10@gmail.com>
2023-11-21 18:31:06 +0530

    Stats Hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm2 from "./components/TextForm2";
 
 function App() {
   return (
     <>
-      {/* <Navbar title={123} aboutText={123}/> */}
-      <Navbar />
+      <Navbar title="TextUtils" />
+      <div className="container my-3">
+
+        <TextForm2 heading="Enter text to analyze"/>
+      </div>
     </>
   );
 }
